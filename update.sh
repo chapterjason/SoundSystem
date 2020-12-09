68aa1f8bd53045bc33d0fd3543606d0326be0682
chapterjason <jason@sourecode.dev>
2020-12-10 00:43:28 +0100

    Add update scripts

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+git add -A
+git stash
+git pull --rebase
+git stash pop
+
+"SCRIPT_DIRECTORY/build.sh"
+
+supervisorctl stop server
+supervisorctl start server
+
