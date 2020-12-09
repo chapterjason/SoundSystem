d33b0d3aa6233feabc365ce91358552a01cd4f35
chapterjason <jason@sourecode.dev>
2020-12-10 00:18:56 +0100

    Fix scripts script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,11 @@ SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pw
 
 echo "Rebuild scripts..."
 
+# Delete old file
+if test -f "$SCRIPT_DIRECTORY/dist/scripts.zip"; then
+  rm "$SCRIPT_DIRECTORY/dist/scripts.zip"
+fi
+
 mkdir -p "$SCRIPT_DIRECTORY/dist"
 
 # Zip new file
@@ -12,11 +17,3 @@ mv src scripts
 zip -r ./dist/scripts.zip ./scripts/*
 mv scripts src
 popd || exit;
-
-# Delete old file
-if test -f "$SCRIPT_DIRECTORY/dist/scripts.zip"; then
-  rm "$SCRIPT_DIRECTORY/dist/scripts.zip"
-fi
-
-# Move script into dist
-mv "$SCRIPT_DIRECTORY/scripts.zip" "$SCRIPT_DIRECTORY/dist"
