7bc5812b207a6fcca91b7a8af4f1b858d8ad1b80
chapterjason <jason@sourecode.dev>
2020-12-10 00:44:16 +0100

    Fix build

--- ui/src/Component/Node/Node.tsx
+++ ui/src/Component/Node/Node.tsx
@@ -50,7 +50,7 @@ export function Node(props: NodeProps) {
                         {setModeVisible && <SetMode id={id} node={node}/>}
                     </InputGroup.Prepend>
                     <InputGroup.Append>
-                        <InputGroup.Text classNames={modeTextClasses}>{mode}</InputGroup.Text>
+                        <InputGroup.Text className={modeTextClasses}>{mode}</InputGroup.Text>
                     </InputGroup.Append>
                 </InputGroup>
             </td>
