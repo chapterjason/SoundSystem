dc537a9cc9853a378654d0ef496aad3b471a3263
chapterjason <jason@sourecode.dev>
2020-12-10 01:04:43 +0100

    Fix client script

--- client/src/Utils/Update.ts
+++ client/src/Utils/Update.ts
@@ -1,7 +1,7 @@
 import { Process } from "@mscs/process";
 
 export async function update(){
-    const updateProcess = new Process(["bash", "/home/pi/scripts/client/client_update.sh"], {
+    const updateProcess = new Process(["/home/pi/scripts/client/client_update.sh"], {
         detached: true,
     });
 
