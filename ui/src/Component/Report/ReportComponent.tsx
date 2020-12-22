df10f332585f655bfb7844416907559af6df8eee
chapterjason <jason@sourecode.dev>
2020-12-22 23:03:19 +0100

    Add ui report

--- ui/src/Component/Report/ReportComponent.tsx
+++ ui/src/Component/Report/ReportComponent.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import { useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { useActions, useValues } from "kea";
 import { ReportLogic } from "./ReportLogic";
+import "./report.scss";
+import { HorizontalGridLines, VerticalBarSeries, VerticalGridLines, XAxis, XYPlot, YAxis } from "react-vis";
 
 export function ReportComponent() {
     const { reports, autoRefresh, updated, requestTime, timeout } = useValues(ReportLogic);
@@ -22,11 +24,30 @@ export function ReportComponent() {
 
     return (
         <Container>
-            <pre>
-                <code>
-                    {JSON.stringify(reports, null, "  ")}
-                </code>
-            </pre>
+            <Row>
+                <Col>
+                    <XYPlot width={300} height={300} stackBy="y" xType="time">
+                        <VerticalGridLines/>
+                        <HorizontalGridLines/>
+                        <XAxis/>
+                        <YAxis/>
+                        {reports.map(report => {
+                            const data = [...report.packets]
+                                .map((packet, index) => {
+                                    return {
+                                        x: new Date(packet.timestamp),
+                                        y: index,
+                                    };
+                                }) as any;
+
+                            return (
+                                <VerticalBarSeries barWidth={20} data={data}/>
+                            );
+                        })}
+                    </XYPlot>
+                </Col>
+            </Row>
+            <pre><code>{JSON.stringify(reports, null, "  ")}</code></pre>
         </Container>
     );
 }
