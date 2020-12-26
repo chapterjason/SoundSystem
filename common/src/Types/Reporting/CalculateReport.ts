b259701872ccc637aa234170c29178d3024f47c0
chapterjason <jason@sourecode.dev>
2020-12-26 20:34:32 +0100

    Fix client

--- common/src/Types/Reporting/CalculateReport.ts
+++ common/src/Types/Reporting/CalculateReport.ts
@@ -10,7 +10,7 @@ export function calculateReport(points: ReportingPoint[]) {
     const first = points[0];
     const last = points[points.length - 1];
 
-    const networkCommand = NetworkCommand.parse(Buffer.from(first.data, "base64"));
+    const networkCommand = NetworkCommand.parse(Buffer.from(Buffer.from(first.data, "base64").toString()));
 
     const report: Report = {
         id: networkCommand.getCommand(),
