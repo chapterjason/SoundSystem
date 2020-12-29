0a0a26a7f493fe726b11fd3e5af552139ce2a1d7
chapterjason <jason@sourecode.dev>
2020-12-29 17:09:49 +0100

    Fix runtime

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -26,7 +26,6 @@ module.exports = {
                         "@babel/preset-typescript",
                     ],
                     plugins: [
-                        "@babel/plugin-transform-runtime",
                         "@babel/plugin-syntax-dynamic-import",
                         "@babel/plugin-proposal-class-properties"
                     ]
