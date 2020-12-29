ac06210d478a8e5c1522f2782498589ec506c560
chapterjason <jason@sourecode.dev>
2020-12-29 16:08:28 +0100

    Remove old exports

--- common/src/index.ts
+++ common/src/index.ts
@@ -7,10 +7,4 @@ export * from "./Node/Types/Stream";
 export * from "./Node/Types/SoundNodeData";
 export * from "./Node/Types/SoundNodeResponseData";
 
-export * from "./Reporting/Types/ReportingPoint";
-export * from "./Reporting/Types/Report";
-
-export * from "./Reporting/CalculateReport";
-export * from "./Reporting/CalculateReports";
-
 export * from "./Utils/Base64";
