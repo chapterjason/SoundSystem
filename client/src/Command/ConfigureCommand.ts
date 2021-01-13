45b81dfeabeae8789d4aec92fee9f82d757373fe
chapterjason <jason@sourecode.dev>
2021-01-14 00:21:12 +0100

    Fix configure command

--- client/src/Command/ConfigureCommand.ts
+++ client/src/Command/ConfigureCommand.ts
@@ -1,9 +1,9 @@
-import { Command as ConsoleCommand, InputInterface, InputOptions, OptionMode, OutputInterface, StyledOutput } from "@mscs/console";
+import { ArgumentMode, Command as ConsoleCommand, InputArguments, InputInterface, OutputInterface, StyledOutput } from "@mscs/console";
 import { promises as fs } from "fs";
 import { Process } from "@mscs/process";
 import { Inject, Service } from "@soundsystem/system";
 
-interface ConfigureCommandOptions extends InputOptions {
+interface ConfigureCommandArguments extends InputArguments {
     hostname: string;
 
     host: string;
@@ -14,7 +14,7 @@ interface ConfigureCommandOptions extends InputOptions {
 }
 
 @Service("command.configure", { tags: ["command"] })
-export class ConfigureCommand extends ConsoleCommand<{}, ConfigureCommandOptions> {
+export class ConfigureCommand extends ConsoleCommand<ConfigureCommandArguments> {
 
     protected environmentFile: string;
 
@@ -25,25 +25,26 @@ export class ConfigureCommand extends ConsoleCommand<{}, ConfigureCommandOptions
 
     protected configure(): void {
         this.setName("configure")
-            .addOption("hostname", null, OptionMode.VALUE_REQUIRED, "Hostname of this node")
-            .addOption("host", null, OptionMode.VALUE_REQUIRED, "Host of the main server")
-            .addOption("port", null, OptionMode.VALUE_REQUIRED, "Port of the main server")
-            .addOption("servicePort", null, OptionMode.VALUE_REQUIRED, "Service port for main server communications")
+            .addArgument("hostname", ArgumentMode.REQUIRED, "Hostname of this node")
+            .addArgument("host", ArgumentMode.REQUIRED, "Host of the main server")
+            .addArgument("port", ArgumentMode.REQUIRED, "Port of the main server")
+            .addArgument("servicePort", ArgumentMode.REQUIRED, "Service port for main server communications")
         ;
     }
 
-    protected async execute(input: InputInterface, output: OutputInterface): Promise<number> {
-        const host = input.getOption("host") as string;
-        const port = input.getOption("port") as string;
-        const servicePort = input.getOption("servicePort") as string;
+    protected async execute(input: InputInterface<ConfigureCommandArguments>, output: OutputInterface): Promise<number> {
+        const io = new StyledOutput(input, output);
+
+        const host = input.getArgument("host");
+        const port = input.getArgument("port");
+        const servicePort = input.getArgument("servicePort");
 
         await fs.writeFile(this.environmentFile, `HOST=${host}
 PORT=${port}
 SERVICE_PORT=${servicePort}`);
 
-
         // Set Hostname
-        const hostname = input.getOption("hostname") as string;
+        const hostname = input.getArgument("hostname");
 
         const setHostnameProcess = new Process(["sudo", "raspi-config", "nonint", "do_hostname", hostname]);
         await setHostnameProcess.mustRun();
