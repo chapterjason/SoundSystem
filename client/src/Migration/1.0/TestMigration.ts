a4758fe48d94eb4c27c8276b0a8ee44f9e549b7e
chapterjason <jason@sourecode.dev>
2021-01-14 00:46:31 +0100

    Fix test migration tag

--- client/src/Migration/1.0/TestMigration.ts
+++ client/src/Migration/1.0/TestMigration.ts
@@ -2,7 +2,7 @@ import { AbstractMigration } from "@soundsystem/migration";
 import * as fs from "fs";
 import { Service } from "@soundsystem/system";
 
-@Service("migration.migrations.1-0.test", {tags: ["migrations"]})
+@Service("migration.migrations.1-0.test", {tags: ["migration"]})
 export class TestMigration extends AbstractMigration {
 
     public getVersion(): string {
