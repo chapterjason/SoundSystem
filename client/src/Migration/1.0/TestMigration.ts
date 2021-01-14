8c0881fb6b53ba8318b197b52a8d0dcd302b5c6b
chapterjason <jason@sourecode.dev>
2021-01-14 08:49:54 +0100

    Fix test migration

--- client/src/Migration/1.0/TestMigration.ts
+++ client/src/Migration/1.0/TestMigration.ts
@@ -1,6 +1,8 @@
 import { AbstractMigration } from "@soundsystem/migration";
 import * as fs from "fs";
 import { Service } from "@soundsystem/system";
+import * as os from "os";
+import path from "path";
 
 @Service("migration.migrations.1-0.test", {tags: ["migration"]})
 export class TestMigration extends AbstractMigration {
@@ -9,11 +11,15 @@ export class TestMigration extends AbstractMigration {
         return "20210114124120";
     }
 
+    private getFile(){
+        return path.join(os.homedir(), "test.md");
+    }
+
     public async up(): Promise<void> {
-        fs.writeFileSync("~/test.md", "# hello");
+        fs.writeFileSync(this.getFile(), "# hello");
     }
 
     public async down(): Promise<void> {
-        fs.unlinkSync("~/test.md");
+        fs.unlinkSync(this.getFile());
     }
 }
